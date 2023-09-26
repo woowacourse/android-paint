ebe5f2fa1071986c6af0dfa4289991c3a0a82ab9
s9hn <tnghqn@naver.com>
2023-09-27 01:38:09 +0900

    refactor: 코드 리팩터링 및 주석설명 추가

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -1,7 +1,6 @@
 package woowacourse.paint
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.slider.RangeSlider
@@ -12,38 +11,46 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val colorAdapter by lazy { ColorAdapter(::selectColor, this) }
 
+    private fun selectColor(color: Int) {
+        binding.dpMain.changeColor(color)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         attachColorAdapter()
-
-        binding.btnMainColor.setOnClickListener {
-            binding.rvMainColor.visibility = View.VISIBLE
-
-        }
-
-        binding.btnMainWidth.setOnClickListener {
-            binding.rsMainWidth.visibility = View.VISIBLE
-        }
-
-
-
-        binding.rsMainWidth.valueFrom = MIN_VALUE
-        binding.rsMainWidth.valueTo = MAX_VALUE
-        binding.rsMainWidth.addOnChangeListener(RangeSlider.OnChangeListener { _, value, _ ->
-            binding.dpMain.changeWidth(value)
-        })
-    }
-
-    private fun selectColor(color: Int) {
-        Log.d("123123", color.toString())
-        binding.dpMain.changeColor(color)
+        setupButton()
+        setupRangeSlider()
     }
 
     private fun attachColorAdapter() {
         binding.rvMainColor.adapter = colorAdapter
         binding.rvMainColor.setHasFixedSize(true)
+    }
+
+    private fun setupButton() {
+        binding.btnMainColor.setOnClickListener { setVisibility(binding.rvMainColor) }
+        binding.btnMainWidth.setOnClickListener { setVisibility(binding.rsMainWidth) }
+    }
+
+    private fun setVisibility(view: View) {
+        when (view.visibility == View.GONE) {
+            true -> view.visibility = View.VISIBLE
+            false -> view.visibility = View.GONE
+        }
+    }
+
+    private fun setupRangeSlider() {
+        binding.rsMainWidth.valueFrom = MIN_VALUE
+        binding.rsMainWidth.valueTo = MAX_VALUE
+        selectWidth()
+    }
+
+    private fun selectWidth() {
+        binding.rsMainWidth.addOnChangeListener(RangeSlider.OnChangeListener { _, value, _ ->
+            binding.dpMain.changeWidth(value)
+        })
     }
 
     companion object {
