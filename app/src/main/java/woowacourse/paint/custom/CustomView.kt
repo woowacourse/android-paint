55cb1f70a6d506fc8596e559688c09a75c1eff9a
krrong <khk9664@naver.com>
2023-09-26 16:50:25 +0900

    feat: drawPath에서 drawLine로 수정

--- app/src/main/java/woowacourse/paint/custom/CustomView.kt
+++ app/src/main/java/woowacourse/paint/custom/CustomView.kt
@@ -1,9 +1,9 @@
 package woowacourse.paint.custom
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Path
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -13,9 +13,8 @@ class CustomView(
     attributeSet: AttributeSet,
 ) : View(context, attributeSet) {
 
-    private val path = Path()
+    private val points = mutableListOf<Point>()
     private val paint = Paint()
-    private var ovalSize = 0
 
     init {
         isFocusable = true
@@ -24,41 +23,41 @@ class CustomView(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawPath(path, paint)
+
+        val size = points.size
+        for (index in 1 until size) {
+            paint.color = points[index].color
+            paint.strokeWidth = points[index].strokeWidth
+            canvas.drawLine(
+                points[index - 1].x,
+                points[index - 1].y,
+                points[index].x,
+                points[index].y,
+                paint,
+            )
+        }
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val pointX = event.x
         val pointY = event.y
 
         when (event.action) {
-            MotionEvent.ACTION_DOWN -> { addOvalToPath(pointX, pointY) }
-            MotionEvent.ACTION_MOVE -> { addOvalToPath(pointX, pointY) }
+            MotionEvent.ACTION_DOWN -> { points.add(Point(pointX, pointY, paint.strokeWidth, paint.color)) }
+            MotionEvent.ACTION_MOVE -> { points.add(Point(pointX, pointY, paint.strokeWidth, paint.color)) }
+            MotionEvent.ACTION_UP -> { points.removeLast() }
             else -> super.onTouchEvent(event)
         }
         invalidate()
         return true
     }
 
-    private fun addOvalToPath(x: Float, y: Float) {
-        path.addOval(
-            x - (ovalSize / 2),
-            y - (ovalSize / 2),
-            x + (ovalSize / 2),
-            y + (ovalSize / 2),
-            Path.Direction.CW,
-        )
-    }
-
     fun changeThickness(new: Float) {
-        ovalSize = (new * NORMALIZATION).toInt()
+        paint.strokeWidth = new * 100
     }
 
     fun changeColor(new: Int) {
         paint.color = new
-    }
-
-    companion object {
-        const val NORMALIZATION = 100
     }
 }
