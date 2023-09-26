e361c2d32443647125440d81bebc1e4276403e92
Sujin Kim <sujinkim919@gmail.com>
2023-09-26 20:55:21 +0900

    feat: main 화면 뷰 (xml) 구현

---
+++ app/src/main/java/woowacourse/paint/custom/PaintBoard.kt
@@ -0,0 +1,7 @@
+package woowacourse.paint.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PaintBoard constructor(context: Context, attrs: AttributeSet) : View(context, attrs)
