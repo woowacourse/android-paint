3caaac8a7faae1439514d503f9e708eda549561f
s9hn <tnghqn@naver.com>
2023-09-27 00:14:52 +0900

    feat: canvas 구현

---
+++ app/src/main/java/woowacourse/paint/DrawingPaper.kt
@@ -0,0 +1,76 @@
+package woowacourse.paint
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingPaper(
+    context: Context, attributeSet: AttributeSet
+) : View(context, attributeSet) {
+
+    private val pathRecords: MutableList<Path> = mutableListOf(Path())
+    private val paintRecords: MutableList<Paint> = mutableListOf(Paint())
+    private var brushWidth = 0f
+
+
+    fun setWidth(width: Float) {
+        val newPath = Path()
+        val newPaint = Paint()
+
+        pathRecords.add(newPath)
+        paintRecords.add(newPaint)
+        brushWidth = width
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        for (record in paintRecords.indices) {
+            val path = pathRecords[record]
+            val paint = paintRecords[record].setBrush()
+
+            paintRecords.last().strokeWidth = brushWidth
+            paintRecords.last().color = Color.GREEN
+
+            canvas.drawPath(path, paint)
+        }
+    }
+
+    private fun Paint.setBrush(): Paint = apply {
+        style = Paint.Style.STROKE
+        strokeCap = Paint.Cap.ROUND
+        strokeJoin = Paint.Join.ROUND
+        strokeMiter = SOFT_ANGLE
+        isAntiAlias = true
+    }
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                pathRecords.last().moveTo(event.x, event.y)
+                pathRecords.last().lineTo(event.x, event.y)
+
+                pathRecords.last().moveTo(event.x, event.y)
+            }
+
+            MotionEvent.ACTION_MOVE -> pathRecords.last().lineTo(event.x, event.y)
+            else -> super.onTouchEvent(event)
+        }
+
+        invalidate()
+        return true
+    }
+
+    companion object {
+        private const val SOFT_ANGLE = 0f
+    }
+
+}
--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -2,13 +2,25 @@ package woowacourse.paint
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.RangeSlider
+import woowacourse.paint.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+
+        binding.rsMain.valueFrom = 0.0f
+        binding.rsMain.valueTo = 100.0f
+
+        binding.rsMain.addOnChangeListener(RangeSlider.OnChangeListener { _, value, _ ->
+            binding.dpMain.setWidth(value)
+        })
     }
 }
+
