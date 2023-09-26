9b1f379f140f57069bbbde05450716e92a997928
buna <tmdgh1592@naver.com>
2023-09-27 02:13:10 +0900

    feat: 메인 액티비티에 캔버스, 팔레트 화면 구현

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -2,13 +2,26 @@ package woowacourse.paint
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import woowacourse.paint.view.CanvasView
+import woowacourse.paint.view.PaletteColor
+import woowacourse.paint.view.PaletteView
 
 class MainActivity : AppCompatActivity() {
+    private val canvasView: CanvasView by lazy { findViewById(R.id.canvas_view) }
+    private val paletteView: PaletteView by lazy { findViewById(R.id.palette_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        paletteView.setOnPropertyChangeListener(object : PaletteView.OnPaintPropertyChangeListener {
+            override fun onColorSelected(paintColor: PaletteColor) {
+                canvasView.setPaintColor(paintColor)
+            }
+
+            override fun onStrokeThicknessChanged(paintThickness: Float) {
+                canvasView.setPaintThickness(paintThickness)
+            }
+        })
     }
 }
