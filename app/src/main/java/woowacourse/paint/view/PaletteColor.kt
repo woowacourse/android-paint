da6faf2ece9dc32892191fd2ff45f07f7afce28b
buna <tmdgh1592@naver.com>
2023-09-27 01:00:02 +0900

    feat: 색상을 나타내는 뷰 구현

---
+++ app/src/main/java/woowacourse/paint/view/PaletteColor.kt
@@ -0,0 +1,16 @@
+package woowacourse.paint.view
+
+enum class PaletteColor(val color: Int) {
+    RED(0xFFFF0000.toInt()),
+    ORANGE(0xFFFFA500.toInt()),
+    YELLOW(0xFFFFFF00.toInt()),
+    GREEN(0xFF008000.toInt()),
+    BLUE(0xFF0000FF.toInt()),
+    ;
+
+    companion object {
+        fun forEach(action: (PaletteColor) -> Unit) {
+            for (value in values()) action(value)
+        }
+    }
+}
