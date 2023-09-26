443ab61f2ea21582683e01782a6c14a555db2dcf
Sujin Kim <sujinkim919@gmail.com>
2023-09-26 22:08:15 +0900

    feat: 자유 곡선 그리기 구현

--- app/src/main/java/woowacourse/paint/custom/PaintBoard.kt
+++ app/src/main/java/woowacourse/paint/custom/PaintBoard.kt
@@ -1,7 +1,42 @@
 package woowacourse.paint.custom
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
-class PaintBoard constructor(context: Context, attrs: AttributeSet) : View(context, attrs)
+class PaintBoard constructor(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private val path = Path()
+    private val paint = Paint()
+
+    init {
+        paint.color = Color.BLUE
+        paint.strokeWidth = 5f
+        paint.style = Paint.Style.STROKE
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawPath(path, paint)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                path.moveTo(event.x, event.y)
+                return true
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                path.lineTo(event.x, event.y)
+            }
+
+            else -> return false
+        }
+        invalidate()
+        return true
+    }
+}
