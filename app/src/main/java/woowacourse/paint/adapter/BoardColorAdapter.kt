099c05b0a819870d64cef22c360f2b79194fc942
mendel <rhthrhrl0@naver.com>
2023-09-27 17:27:35 +0900

    feat: Color 어댑터와 뷰홀더 추가

---
+++ app/src/main/java/woowacourse/paint/adapter/BoardColorAdapter.kt
@@ -0,0 +1,40 @@
+package woowacourse.paint.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import woowacourse.model.BoardColor
+
+class BoardColorAdapter(
+    private val onColorClick: (BoardColor) -> Unit,
+) : ListAdapter<BoardColor, BoardColorViewHolder>(ColorDiffUtil) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BoardColorViewHolder {
+        return BoardColorViewHolder.create(parent, onColorClick)
+    }
+
+    override fun onBindViewHolder(holder: BoardColorViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    fun changeColorList(colors: List<BoardColor>) {
+        submitList(colors)
+    }
+
+    companion object {
+        private val ColorDiffUtil = object : DiffUtil.ItemCallback<BoardColor>() {
+            override fun areItemsTheSame(
+                oldItem: BoardColor,
+                newItem: BoardColor,
+            ): Boolean {
+                return oldItem == newItem
+            }
+
+            override fun areContentsTheSame(
+                oldItem: BoardColor,
+                newItem: BoardColor,
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
---
+++ app/src/main/java/woowacourse/paint/adapter/BoardColorViewHolder.kt
@@ -0,0 +1,28 @@
+package woowacourse.paint.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import woowacourse.model.BoardColor
+import woowacourse.paint.databinding.ItemMainBoardColorBinding
+
+class BoardColorViewHolder(
+    private val binding: ItemMainBoardColorBinding,
+    onColorClick: (BoardColor) -> Unit,
+) : ViewHolder(binding.root) {
+    init {
+        binding.onColorClickListener = onColorClick
+    }
+
+    fun bind(color: BoardColor) {
+        binding.color = color
+    }
+
+    companion object {
+        fun create(parent: ViewGroup, onColorClick: (BoardColor) -> Unit): BoardColorViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemMainBoardColorBinding.inflate(layoutInflater, parent, false)
+            return BoardColorViewHolder(binding, onColorClick)
+        }
+    }
+}
