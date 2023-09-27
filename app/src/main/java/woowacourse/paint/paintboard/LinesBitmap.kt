dfbea62f4cc67326269ab870b5f16d316f3cdd93
briandr97 <briandr@naver.com>
2023-09-28 03:10:45 +0900

    feat: 이전 비트맵이 활용되는 이슈 해결

--- app/src/main/java/woowacourse/paint/paintboard/LinesBitmap.kt
+++ app/src/main/java/woowacourse/paint/paintboard/LinesBitmap.kt
@@ -4,13 +4,21 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
 
-class LinesBitmap(w: Int, h: Int, canvasPaint: Paint) {
-    private val bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-    private val canvas = Canvas(bitmap).apply { drawPaint(canvasPaint) }
+class LinesBitmap(private val w: Int, private val h: Int, private val canvasPaint: Paint) {
     fun create(lines: Lines): Bitmap {
+        val bitmap = getBitmap()
+        val canvas = getCanvas(bitmap)
         lines.forEach {
             canvas.drawPath(it.path, it.paint)
         }
         return bitmap
+    }
+
+    private fun getBitmap(): Bitmap {
+        return Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+    }
+
+    private fun getCanvas(bitmap: Bitmap): Canvas {
+        return Canvas(bitmap).apply { drawPaint(canvasPaint) }
     }
 }
