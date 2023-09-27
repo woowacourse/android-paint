9a8fe4a9c855354e35cfae37dac456ea565ee350
ippnsj <ippnsj@gmail.com>
2023-09-27 18:18:31 +0900

    feat: 그림판 뷰 추가

---
+++ app/src/main/java/woowacourse/paint/PaintBoard.kt
@@ -0,0 +1,7 @@
+package woowacourse.paint
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PaintBoard(context: Context, attrs: AttributeSet) : View(context, attrs)
