a7783a6591d7c473e0bd011022b6320dca2322f4
ippnsj <ippnsj@gmail.com>
2023-09-28 01:17:44 +0900

    feat: 붓으로 캔버스에 그림 그리는 기능 구현

--- app/src/main/java/woowacourse/paint/PaintBoard.kt
+++ app/src/main/java/woowacourse/paint/PaintBoard.kt
@@ -1,7 +1,46 @@
 package woowacourse.paint
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
-class PaintBoard(context: Context, attrs: AttributeSet) : View(context, attrs)
+class PaintBoard(context: Context, attrs: AttributeSet) : View(context, attrs) {
+    private val path = Path()
+    private val paint = Paint().apply {
+        color = Color.RED
+        style = Paint.Style.STROKE
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawPath(path, paint)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                path.moveTo(event.x, event.y)
+                true
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                path.lineTo(event.x, event.y)
+                true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                invalidate()
+                true
+            }
+
+            else -> super.onTouchEvent(event)
+        }
+    }
+}
