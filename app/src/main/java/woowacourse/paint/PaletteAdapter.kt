a18c98bbb6505633754945091a68db501aa5e151
inseonyun <yuninseon@naver.com>
2023-09-28 23:26:49 +0900

    refactor: 단일 표현식으로 수정

--- app/src/main/java/woowacourse/paint/PaletteAdapter.kt
+++ app/src/main/java/woowacourse/paint/PaletteAdapter.kt
@@ -7,9 +7,8 @@ class PaletteAdapter(
     private val items: List<Int>,
     private val onPaletteClick: (Int) -> Unit,
 ) : RecyclerView.Adapter<PaletteViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaletteViewHolder {
-        return PaletteViewHolder(parent, onPaletteClick)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaletteViewHolder =
+        PaletteViewHolder(parent, onPaletteClick)
 
     override fun onBindViewHolder(holder: PaletteViewHolder, position: Int) {
         holder.bind(items[position])
