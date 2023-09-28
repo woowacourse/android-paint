b17ec06d0a83b52985895c15e57fe6ee01b869ca
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-09-28 18:47:58 +0900

    feat: MainActivity 화면 구성

---
+++ app/src/main/java/woowacourse/paint/FreeDrawView.kt
@@ -0,0 +1,7 @@
+package woowacourse.paint
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class FreeDrawView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet)
