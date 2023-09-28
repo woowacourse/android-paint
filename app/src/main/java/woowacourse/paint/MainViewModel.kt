9f797c6842fd5574d766d48494528ec72ebc446d
inseonyun <yuninseon@naver.com>
2023-09-29 00:08:17 +0900

    refactor: viewModel setColors 수정

--- app/src/main/java/woowacourse/paint/MainViewModel.kt
+++ app/src/main/java/woowacourse/paint/MainViewModel.kt
@@ -3,12 +3,13 @@ package woowacourse.paint
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    private var _colors: MutableList<Int> = mutableListOf()
+    private val _colors: MutableList<Int> = mutableListOf()
     val colors: List<Int> get() = _colors.toList()
     var strokeSize: Float = 0.0f
     var paintColor: Int = 0
 
     fun setColors(colors: List<Int>) {
-        _colors = colors.toMutableList()
+        _colors.clear()
+        _colors.addAll(colors)
     }
 }
