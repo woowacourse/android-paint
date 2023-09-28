6da0e4a30e32eaddd59fc745abfa0801621a7cad
vrexpert <vrexpert2302@gmail.com>
2023-09-29 00:44:40 +0900

    feat(MainActivity): ViewModel 사용 및 상태 소비 로직 작성

--- app/src/main/java/woowacourse/paint/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/presentation/ui/main/MainActivity.kt
@@ -1,14 +1,51 @@
 package woowacourse.paint.presentation.ui.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.material.slider.RangeSlider
+import kotlinx.coroutines.launch
 import woowacourse.paint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initCollect()
+        initView()
+    }
+
+    private fun initCollect() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.line.collect { line ->
+                    binding.cvCanvas.setLine(line)
+                }
+            }
+        }
+    }
+
+    private fun initView() {
+        initRangeWidth()
+        binding.rsWidth.addOnChangeListener(
+            RangeSlider.OnChangeListener { _, value, _ ->
+                viewModel.changeLineWidth(value)
+            },
+        )
+    }
+
+    private fun initRangeWidth() {
+        val line = viewModel.line.value
+        binding.rsWidth.valueFrom = line.minWidth
+        binding.rsWidth.valueTo = line.maxWidth
+        binding.rsWidth.setValues(line.width)
     }
 }
