cbb34caba919a3ee6e432f32a546c59b320a9af4
vrexpert <vrexpert2302@gmail.com>
2023-09-29 00:42:04 +0900

    feat: domain Module 사용

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,14 @@ android {
 }
 
 dependencies {
+    // domain
+    implementation(project(":domain"))
+
+    // viewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.activity:activity-ktx:1.7.2")
+    implementation("androidx.fragment:fragment-ktx:1.6.0")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.0")
     implementation("com.google.android.material:material:1.7.0")
