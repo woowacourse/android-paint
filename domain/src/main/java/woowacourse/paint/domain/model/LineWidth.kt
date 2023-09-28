7c8c3c8f26d23260fdbc5aefb3d8993a7d3515a1
vrexpert <vrexpert2302@gmail.com>
2023-09-28 21:59:02 +0900

    feat(LineWidth): 선 두께 기능 작성

---
+++ domain/src/main/java/woowacourse/paint/domain/model/LineWidth.kt
@@ -0,0 +1,13 @@
+package woowacourse.paint.domain.model
+
+@JvmInline
+value class LineWidth(val value: Float = 30f) {
+    init {
+        require(value in MIN_WIDTH..MAX_WIDTH) { "굵기가 범위를 벗어났습니다" }
+    }
+
+    companion object {
+        const val MIN_WIDTH = 1f
+        const val MAX_WIDTH = 100f
+    }
+}
