e3ce18b453cf046000b58443dba65732d3a14cc1
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-09-28 18:49:52 +0900

    feat: 어뎁터 연결 및 클릭 이벤트 추가

--- app/src/main/java/woowacourse/paint/FreeDrawView.kt
+++ app/src/main/java/woowacourse/paint/FreeDrawView.kt
@@ -1,7 +1,41 @@
 package woowacourse.paint
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
-class FreeDrawView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet)
+class FreeDrawView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
+
+    private val path = Path()
+    private val paint = Paint()
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                true
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
+    }
+
+    fun updateColor(color: Int) {
+        paint.color = color
+    }
+
+    fun updateThickness() {
+    }
+}
--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -2,13 +2,26 @@ package woowacourse.paint
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import woowacourse.paint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        setAdapter()
+    }
+
+    private fun setAdapter() {
+        binding.rvPalette.adapter = PaletteAdapter(
+            backgroundColors = PaletteColor.values().map {
+                it.convertToArgb(this)
+            },
+        ) { paletteClickEvent(it) }
+    }
+
+    private fun paletteClickEvent(color: Int) {
     }
 }
