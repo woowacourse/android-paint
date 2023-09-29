d8b62f5688665c154f2650c4b262e15dc0431a8c
chws0508 <chws0508@naver.com>
2023-09-29 17:29:25 +0900

    feat: 이전 기록들을 저장할 Paintings 일급 컬렉션 객체 구현

---
+++ app/src/main/java/woowacourse/paint/Paintings.kt
@@ -0,0 +1,24 @@
+package woowacourse.paint
+
+import android.graphics.Canvas
+
+data class Paintings(private val value: MutableList<Painting> = mutableListOf()) {
+
+    fun drawOnCanvas(canvas: Canvas) {
+        value.forEach { painting -> painting.drawOnCanvas(canvas) }
+    }
+
+    fun add(painting: Painting) {
+        value.add(painting)
+    }
+
+    // 나중에 되돌리기 구현할 때 사용
+    fun removeLast() {
+        value.removeLastOrNull()
+    }
+
+    // 나중에 전체 지우기 사용할 때 사용
+    fun reset() {
+        value.clear()
+    }
+}
