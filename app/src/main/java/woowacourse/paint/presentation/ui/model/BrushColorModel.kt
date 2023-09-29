58b4632dd4f8b727714a637d3e32c2d27327a8aa
vrexpert <vrexpert2302@gmail.com>
2023-09-29 16:54:20 +0900

    refactor: 주황색 상수화

--- app/src/main/java/woowacourse/paint/presentation/ui/model/BrushColorModel.kt
+++ app/src/main/java/woowacourse/paint/presentation/ui/model/BrushColorModel.kt
@@ -1,11 +1,14 @@
 package woowacourse.paint.presentation.ui.model
 
 import android.graphics.Color
+import androidx.annotation.ColorInt
 
-enum class BrushColorModel(val value: Int) {
+enum class BrushColorModel(@ColorInt val value: Int) {
     RED(Color.RED),
-    ORANGE(0xFF7F000),
+    ORANGE(COLOR_ORANGE),
     YELLOW(Color.YELLOW),
     GREEN(Color.GREEN),
     BLUE(Color.BLUE),
 }
+
+private const val COLOR_ORANGE = 0xFFFF7F00.toInt()
