7dab885420f82ba5b2c814de7f1b6279f73abf42
chws0508 <chws0508@naver.com>
2023-09-29 15:53:32 +0900

    feat: Painting 객체 구현

---
+++ app/src/main/java/woowacourse/paint/Painting.kt
@@ -0,0 +1,46 @@
+package woowacourse.paint
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+
+data class Painting(
+    val path: Path,
+    val paint: Paint,
+) {
+    constructor(
+        path: Path,
+        paintColor: Int,
+        paintWidth: Float,
+    ) : this(
+        path = path,
+        paint = Paint().apply {
+            color = paintColor
+            strokeWidth = paintWidth
+            style = Paint.Style.STROKE
+            isAntiAlias = true
+            strokeCap = Paint.Cap.ROUND
+            strokeJoin = Paint.Join.ROUND
+        },
+    )
+
+    fun movePath(x: Float, y: Float) {
+        path.moveTo(x, y)
+    }
+
+    fun linePath(x: Float, y: Float) {
+        path.lineTo(x, y)
+    }
+
+    fun changeColor(color: Int) {
+        paint.color = color
+    }
+
+    fun changeWidth(width: Float) {
+        paint.strokeWidth = width
+    }
+
+    fun drawOnCanvas(canvas: Canvas) {
+        canvas.drawPath(path, paint)
+    }
+}
