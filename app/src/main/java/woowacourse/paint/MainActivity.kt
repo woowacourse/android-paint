3420b4d73efd3b5cb43afce2aca001a0fd75dcfc
otter <koreanotter6@gmail.com>
2023-09-30 12:48:55 +0900

    feat: PaintView 추가

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -1,14 +1,13 @@
 package woowacourse.paint
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
-
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
---
+++ app/src/main/java/woowacourse/paint/PaintView.kt
@@ -0,0 +1,53 @@
+package woowacourse.paint
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color.BLACK
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class PaintView(
+    context: Context,
+    attributeSet: AttributeSet,
+) : View(context, attributeSet) {
+
+    private var penSize = 50f
+
+    private val path = Path()
+    private val paint = Paint().apply {
+        color = BLACK
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawPath(path, paint)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val pointX = event.x
+        val pointY = event.y
+
+        when (event.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> addOval(pointX, pointY)
+            else -> super.onTouchEvent(event)
+        }
+
+        invalidate()
+        return true
+    }
+
+    private fun addOval(pointX: Float, pointY: Float) {
+        path.addOval(
+            pointX - penSize / 2,
+            pointY - penSize / 2,
+            pointX + penSize / 2,
+            pointY + penSize / 2,
+            Path.Direction.CW,
+        )
+    }
+}
