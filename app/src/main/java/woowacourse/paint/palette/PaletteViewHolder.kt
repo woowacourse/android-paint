bb66147521be16ae73816ddd447fb5c112405fca
krrong <khk9664@naver.com>
2023-10-01 04:08:34 +0900

    refactor: inflate를 ViewHolder에서 진행하도록 수정

--- app/src/main/java/woowacourse/paint/palette/PaletteViewHolder.kt
+++ app/src/main/java/woowacourse/paint/palette/PaletteViewHolder.kt
@@ -1,11 +1,16 @@
 package woowacourse.paint.palette
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import woowacourse.paint.R
 import woowacourse.paint.databinding.ItemPaletteBinding
 import woowacourse.paint.presentation.uimodel.BrushColorUiModel
 
-class PaletteViewHolder(private val binding: ItemPaletteBinding) :
-    RecyclerView.ViewHolder(binding.root) {
+class PaletteViewHolder(parent: ViewGroup) :
+    RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_palette, parent, false)) {
+
+    private val binding = ItemPaletteBinding.bind(itemView)
 
     fun bind(brushColorUiModel: BrushColorUiModel, onPaletteClick: (BrushColorUiModel) -> Unit) {
         binding.palette.apply {
