605d973f13b407afa00b2e0bccfc2cb36ad91b7b
changhwan <chang5hustle@gmail.com>
2023-09-30 11:33:25 +0900

    [feat] 데이터 바인딩 세팅

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -2,13 +2,17 @@ package woowacourse.paint
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.databinding.DataBindingUtil
+import woowacourse.paint.databinding.ActivityMainBindingImpl
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBindingImpl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
