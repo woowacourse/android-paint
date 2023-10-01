d6fe4cfdb24c52a299a66e93bb888900140607bc
hyunji1203 <hj27720@gmail.com>
2023-10-02 00:59:02 +0900

    refactor: List 타입으로 사용할 수 있도록 수정

--- app/src/main/java/woowacourse/paint/Paintings.kt
+++ app/src/main/java/woowacourse/paint/Paintings.kt
@@ -2,7 +2,7 @@ package woowacourse.paint
 
 class Paintings(item: MutableList<Painting>) {
     private val _paintings = item
-    val painting: List<Painting> = _paintings
+    val painting: List<Painting> = _paintings.toList()
 
     fun storePainting(painting: Painting) {
         _paintings.add(painting)
