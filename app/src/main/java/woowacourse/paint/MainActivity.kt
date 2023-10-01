8c2501e5b12bbc429ba1850ea2436bdfc43b7199
otter <koreanotter6@gmail.com>
2023-10-01 22:30:59 +0900

    refactor: PenToolView 커스텀뷰 작성 및 색상, 굵기 변경 버튼 추가

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -1,14 +1,8 @@
 package woowacourse.paint
 
-import android.graphics.Color
 import android.os.Bundle
-import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MarginLayoutParamsCompat
 import woowacourse.paint.databinding.ActivityMainBinding
-import woowacourse.paint.model.PaletteColor
-import woowacourse.paint.model.pen.Pen
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,38 +13,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initSlider()
-        initColorPalette()
+        initPenTool()
     }
 
-    private fun initSlider() {
-        binding.slider.value = Pen.DEFAULT_WIDTH
-        binding.slider.valueTo = Pen.MAX_WIDTH
-        binding.slider.valueFrom = Pen.MIN_WIDTH
-        binding.slider.stepSize = Pen.WIDTH_STEP
-
-        binding.slider.addOnChangeListener { slider, value, fromUser ->
-            binding.paintView.pen.width = value
+    private fun initPenTool() {
+        val selectedPen = binding.penToolView.selectedPen
+        binding.penToolView.setChangeColorClickEvent {
+            binding.paintView.pen = selectedPen
         }
-    }
-
-    private fun initColorPalette() {
-        PaletteColor.values().forEachIndexed { index, paletteColor ->
-            val container = binding.colorContainer
-            val view = TextView(this@MainActivity)
-            val params = ViewGroup.MarginLayoutParams(240, 240)
-            if (index != PaletteColor.values().lastIndex) {
-                MarginLayoutParamsCompat.setMarginEnd(params, 40)
-            }
-
-            view.layoutParams = params
-            view.setBackgroundColor(Color.parseColor(paletteColor.hexCode))
-
-            view.setOnClickListener {
-                binding.paintView.pen.paletteColor = paletteColor
-            }
-
-            container.addView(view)
+        binding.penToolView.setChangeWidthClickEvent {
+            binding.paintView.pen = selectedPen
         }
     }
 }
---
+++ app/src/main/java/woowacourse/paint/PenToolView.kt
@@ -0,0 +1,77 @@
+package woowacourse.paint
+
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.MarginLayoutParamsCompat
+import woowacourse.paint.databinding.ViewPenToolBinding
+import woowacourse.paint.model.PaletteColor
+import woowacourse.paint.model.pen.BallpointPen
+import woowacourse.paint.model.pen.Pen
+
+class PenToolView(
+    context: Context,
+    attrs: AttributeSet? = null,
+) : ConstraintLayout(context, attrs) {
+
+    private val binding: ViewPenToolBinding =
+        ViewPenToolBinding.inflate(LayoutInflater.from(context), this, true)
+
+    var selectedPen: Pen = BallpointPen()
+        private set
+
+    init {
+        initSlider()
+        initColorPalette()
+    }
+
+    fun setChangeColorClickEvent(f: () -> Unit) {
+        binding.btnChangeColor.setOnClickListener {
+            f()
+        }
+    }
+
+    fun setChangeWidthClickEvent(f: () -> Unit) {
+        binding.btnChangeWidth.setOnClickListener {
+            f()
+        }
+    }
+
+    private fun initSlider() {
+        binding.slider.value = Pen.DEFAULT_WIDTH
+        binding.slider.valueTo = Pen.MAX_WIDTH
+        binding.slider.valueFrom = Pen.MIN_WIDTH
+        binding.slider.stepSize = Pen.WIDTH_STEP
+
+        binding.slider.addOnChangeListener { slider, value, fromUser ->
+            selectedPen = selectedPen.apply {
+                width = value
+            }
+        }
+    }
+
+    private fun initColorPalette() {
+        PaletteColor.values().forEachIndexed { index, paletteColor ->
+            val container = binding.colorContainer
+            val view = TextView(context)
+            val params = MarginLayoutParams(240, 240)
+            if (index != PaletteColor.values().lastIndex) {
+                MarginLayoutParamsCompat.setMarginEnd(params, 40)
+            }
+
+            view.layoutParams = params
+            view.setBackgroundColor(Color.parseColor(paletteColor.hexCode))
+
+            view.setOnClickListener {
+                selectedPen = selectedPen.apply {
+                    this.paletteColor = paletteColor
+                }
+            }
+
+            container.addView(view)
+        }
+    }
+}
