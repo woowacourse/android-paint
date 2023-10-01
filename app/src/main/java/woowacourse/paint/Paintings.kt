030a405c7eaeac8ed4b44f6ad241117ef81aea53
hyunji1203 <hj27720@gmail.com>
2023-10-02 01:01:09 +0900

    fix: 그려진 곡선이 지워지는 오류 수

--- app/src/main/java/woowacourse/paint/Paintings.kt
+++ app/src/main/java/woowacourse/paint/Paintings.kt
@@ -1,8 +1,8 @@
 package woowacourse.paint
 
-class Paintings(item: MutableList<Painting>) {
+class Paintings(item: MutableList<Painting> = mutableListOf()) {
     private val _paintings = item
-    val painting: List<Painting> = _paintings.toList()
+    val painting: List<Painting> = _paintings
 
     fun storePainting(painting: Painting) {
         _paintings.add(painting)
