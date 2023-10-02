751deaffb558d5f95a445a4cc0d07d1aca2f1bec
chws0508 <chws0508@naver.com>
2023-10-03 01:45:42 +0900

    refactor : Paintings가 불변성을 보장 받도록 수정

--- app/src/main/java/woowacourse/paint/Paintings.kt
+++ app/src/main/java/woowacourse/paint/Paintings.kt
@@ -2,29 +2,32 @@ package woowacourse.paint
 
 import android.graphics.Canvas
 
-class Paintings(private val value: MutableList<Painting> = mutableListOf()) {
+class Paintings(initPaintings: List<Painting> = emptyList()) {
+
     constructor(
         initPresentPainting: Painting,
-    ) : this(value = mutableListOf(initPresentPainting))
+    ) : this(initPaintings = mutableListOf(initPresentPainting))
+
+    private val paintings: MutableList<Painting> = initPaintings.toMutableList()
 
     val presentPainting: Painting?
-        get() = value.lastOrNull()
+        get() = paintings.lastOrNull()
 
     fun drawOnCanvas(canvas: Canvas) {
-        value.forEach { painting -> painting.drawOnCanvas(canvas) }
+        paintings.forEach { painting -> painting.drawOnCanvas(canvas) }
     }
 
     fun startNewPainting() {
-        value.add(presentPainting?.getInitializedPathPainting() ?: return)
+        paintings.add(presentPainting?.getInitializedPathPainting() ?: return)
     }
 
     // 나중에 되돌리기 구현할 때 사용
     fun removeLast() {
-        value.removeLastOrNull()
+        paintings.removeLastOrNull()
     }
 
     // 나중에 전체 지우기 사용할 때 사용
     fun reset() {
-        value.clear()
+        paintings.clear()
     }
 }
