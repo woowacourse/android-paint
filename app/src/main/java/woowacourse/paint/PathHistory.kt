f83dc4fed31cca915fa5b03f5437fc1d12de47c3
whk06061 <gju06061@naver.com>
2023-10-05 12:06:20 +0900

    feat: 일급 컬렉션에 대해 깊은 복사 구현

--- app/src/main/java/woowacourse/paint/PathHistory.kt
+++ app/src/main/java/woowacourse/paint/PathHistory.kt
@@ -1,9 +1,11 @@
 package woowacourse.paint
 
 class PathHistory(private val _paths: MutableList<PathPaint> = mutableListOf()) {
-    val paths: List<PathPaint> get() = _paths.toList()
+    val paths: List<PathPaint> get() = _paths.deepCopy()
 
     fun addPath(pathPaint: PathPaint) {
         _paths.add(pathPaint)
     }
+
+    private fun List<PathPaint>.deepCopy() = map { it.copy() }
 }
