3eece510d63ae3d218e65d1be6703e0f159cfcb1
pingu244 <pingu244@naver.com>
2023-10-06 01:18:10 +0900

    [refactor] 깊은 복사

--- app/src/main/java/woowacourse/paint/model/DrawingHistory.kt
+++ app/src/main/java/woowacourse/paint/model/DrawingHistory.kt
@@ -1,10 +1,18 @@
 package woowacourse.paint.model
 
+import android.graphics.Paint
+import android.graphics.Path
+
 class DrawingHistory {
 
     private val _history = mutableListOf<Drawing>()
     val history: List<Drawing>
-        get() = _history.toList()
+        get() = _history.map {
+            it.copy(
+                path = Path(it.path),
+                paint = Paint(it.paint),
+            )
+        }.toList()
 
     fun add(drawing: Drawing) = _history.add(drawing)
 
