ea914faee32345ddf064d952363116168313315f
mendel <rhthrhrl0@naver.com>
2023-10-05 20:30:47 +0900

    feat: BrushViewHolder bind 메소드 수정

--- app/src/main/java/woowacourse/paint/ui/main/brush/BrushTypeViewHolder.kt
+++ app/src/main/java/woowacourse/paint/ui/main/brush/BrushTypeViewHolder.kt
@@ -3,6 +3,8 @@ package woowacourse.paint.ui.main.brush
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import woowacourse.paint.R
+import woowacourse.paint.customView.content.BrushType
 import woowacourse.paint.databinding.ItemMainBrushTypeBinding
 import woowacourse.paint.model.BrushTypeItem
 
@@ -16,6 +18,10 @@ class BrushTypeViewHolder(
 
     fun bind(brushTypeItem: BrushTypeItem) {
         binding.item = brushTypeItem
+        binding.tvBrushType.text = when (brushTypeItem.brushType) {
+            BrushType.Stroke -> binding.root.context.getString(R.string.brush_type_stroke)
+            BrushType.Eraser -> binding.root.context.getString(R.string.brush_type_eraser)
+        }
     }
 
     companion object {
