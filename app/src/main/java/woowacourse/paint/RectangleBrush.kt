87aa780786c87002a25b0a77cccf8547c0182507
EmilyCh0 <emily9803@ewhain.net>
2023-10-05 18:06:43 +0900

    feat: RectangleBrush 구현

---
+++ app/src/main/java/woowacourse/paint/RectangleBrush.kt
@@ -0,0 +1,39 @@
+package woowacourse.paint
+
+import android.graphics.Paint
+import android.graphics.Path
+
+class RectangleBrush(
+    override val path: Path = Path(),
+    override val paint: Paint = Paint(),
+) : Brush {
+
+    private var startX: Float = 0f
+    private var startY: Float = 0f
+
+    override fun setColor(color: Int) {
+        paint.color = color
+    }
+
+    override fun setStrokeWidth(width: Float) {
+        paint.strokeWidth = width
+    }
+
+    override fun startDrawing(x: Float, y: Float) {
+        path.reset()
+        path.moveTo(x, y)
+        startX = x
+        startY = y
+    }
+
+    override fun keepDrawing(x: Float, y: Float) {
+        path.reset()
+        val left = if (startX <= x) startX else x
+        val right = if (startX <= x) x else startX
+        val top = if (startY <= y) startY else y
+        val bottom = if (startY <= y) y else startY
+        path.addRect(left, top, right, bottom, Path.Direction.CCW)
+    }
+
+    override fun finishDrawing() = Unit
+}
