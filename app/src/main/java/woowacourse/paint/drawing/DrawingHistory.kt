f78ed7afde6e0ba8fe59de9dd6e545cbd93ad610
whk06061 <gju06061@naver.com>
2023-10-06 17:05:09 +0900

    feat: DrawingHistory 일급 컬렉션 깊은 복사 구현

--- app/src/main/java/woowacourse/paint/drawing/DrawingHistory.kt
+++ app/src/main/java/woowacourse/paint/drawing/DrawingHistory.kt
@@ -1,5 +1,8 @@
 package woowacourse.paint.drawing
 
+import android.graphics.Paint
+import android.graphics.Path
+
 class DrawingHistory(private val _drawings: MutableList<Drawing> = mutableListOf()) {
     val drawings: List<Drawing> get() = _drawings.deepCopy()
 
@@ -7,5 +10,6 @@ class DrawingHistory(private val _drawings: MutableList<Drawing> = mutableListOf
         _drawings.add(pathPaint)
     }
 
-    private fun List<Drawing>.deepCopy() = map { it.copy() }
+    private fun List<Drawing>.deepCopy() =
+        map { it.copy(path = Path(it.path), paint = Paint(it.paint)) }
 }
