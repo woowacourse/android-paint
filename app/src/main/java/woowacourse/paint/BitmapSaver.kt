e3d11ba16974288df240145c26829ca32e019fe3
briandr97 <briandr@naver.com>
2023-10-07 20:43:12 +0900

    feat: 실패상황에 대해 로그를 찍음

--- app/src/main/java/woowacourse/paint/BitmapSaver.kt
+++ app/src/main/java/woowacourse/paint/BitmapSaver.kt
@@ -1,27 +1,29 @@
 package woowacourse.paint
 
+import android.content.ContentResolver
 import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
 import java.io.OutputStream
 
 class BitmapSaver(private val context: Context) {
     fun save(bitmap: Bitmap, fileName: String): Result<Unit> {
         val contentValues = getContentValues(fileName)
         val contentResolver = context.contentResolver
-        val imageUri: Uri? = contentResolver.insert(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            contentValues,
-        )
-        imageUri?.let {
-            val result = saveBitmap(bitmap, context, it)
-            contentResolver.update(imageUri, contentValues, null, null)
-            if (result) return Result.success(Unit)
+        val imageUri: Uri = getImageUri(contentResolver, contentValues).getOrElse {
+            Log.e(TAG, it.message ?: "")
+            return Result.failure(it)
         }
-        return Result.failure(IllegalStateException("이미지 저장에 실패했습니다"))
+        saveBitmap(bitmap, context, imageUri).getOrElse {
+            Log.e(TAG, it.message ?: "")
+            return Result.failure(it)
+        }
+        contentResolver.update(imageUri, contentValues, null, null)
+        return Result.success(Unit)
     }
 
     private fun getContentValues(fileName: String): ContentValues {
@@ -34,10 +36,27 @@ class BitmapSaver(private val context: Context) {
         }
     }
 
-    private fun saveBitmap(bitmap: Bitmap, context: Context, imageUri: Uri): Boolean {
+    private fun getImageUri(
+        contentResolver: ContentResolver,
+        contentValues: ContentValues,
+    ): Result<Uri> {
+        val imageUri: Uri = contentResolver.insert(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            contentValues,
+        ) ?: return Result.failure(IllegalStateException("이미지 uri를 가져오지 못했습니다"))
+        return Result.success(imageUri)
+    }
+
+    private fun saveBitmap(bitmap: Bitmap, context: Context, imageUri: Uri): Result<Unit> {
         val outputStream: OutputStream? = context.contentResolver.openOutputStream(imageUri)
-        val result = bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        val result = bitmap.compress(Bitmap.CompressFormat.JPEG, COMPRESS_RATE, outputStream)
         outputStream?.close()
-        return result
+        if (!result) return Result.failure(IllegalStateException("비트맵을 저장하지 못했습니다"))
+        return Result.success(Unit)
+    }
+
+    companion object {
+        private const val TAG = "BitmapSaver"
+        private const val COMPRESS_RATE = 100
     }
 }
