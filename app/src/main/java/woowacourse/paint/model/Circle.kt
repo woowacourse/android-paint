31c3e557086ef4cfe2c7d53d7000e9caf16261dc
inseonyun <yuninseon@naver.com>
2023-10-08 12:56:48 +0900

    refactor: Circle radius 수정

--- app/src/main/java/woowacourse/paint/model/Circle.kt
+++ app/src/main/java/woowacourse/paint/model/Circle.kt
@@ -2,9 +2,11 @@ package woowacourse.paint.model
 
 import kotlin.math.abs
 import kotlin.math.min
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class Circle(currentX: Float, currentY: Float, x: Float, y: Float) : Shape {
     val centerX: Float = min(currentX, x) + abs(x - currentX).div(2)
     val centerY: Float = min(currentY, y) + abs(y - currentY).div(2)
-    val radius: Float = abs(centerX - currentX)
+    val radius: Float = sqrt(abs(x - centerX).pow(2) + abs(y - centerY).pow(2))
 }
