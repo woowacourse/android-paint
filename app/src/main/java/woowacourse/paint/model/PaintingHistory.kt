3e30e797b6640e5a009e4dba0189dcc98bcc00c7
Choisehyeon <50761690+Choisehyeon@users.noreply.github.com>
2023-10-08 15:06:14 +0900

    refactor: 변수명 수정

--- app/src/main/java/woowacourse/paint/model/PaintingHistory.kt
+++ app/src/main/java/woowacourse/paint/model/PaintingHistory.kt
@@ -3,31 +3,31 @@ package woowacourse.paint.model
 import java.util.Stack
 
 class PaintingHistory {
-    private val history = mutableListOf<PaintingElement>()
+    private val undoHistory = mutableListOf<PaintingElement>()
     private val redoHistory: Stack<PaintingElement> = Stack()
 
     fun addHistory(track: PaintingElement) {
-        history.add(track)
+        undoHistory.add(track)
     }
 
     fun forEach(action: (PaintingElement) -> Unit) {
-        history.forEach(action)
+        undoHistory.forEach(action)
     }
 
     fun clear() {
-        history.clear()
+        undoHistory.clear()
         redoHistory.clear()
     }
 
     fun undo() {
-        if (history.isNotEmpty()) {
-            redoHistory.add(history.removeLast())
+        if (undoHistory.isNotEmpty()) {
+            redoHistory.add(undoHistory.removeLast())
         }
     }
 
     fun redo() {
         if (redoHistory.isNotEmpty()) {
-            history.add(redoHistory.pop())
+            undoHistory.add(redoHistory.pop())
         }
     }
 }
