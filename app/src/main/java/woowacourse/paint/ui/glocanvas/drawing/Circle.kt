c3797d63eb4e96cad5ef02ca4fdc0160e2b44857
ippnsj <ippnsj@gmail.com>
2023-10-10 02:41:58 +0900

    refactor: 원 시작점 변수명 변경

--- app/src/main/java/woowacourse/paint/ui/glocanvas/drawing/Circle.kt
+++ app/src/main/java/woowacourse/paint/ui/glocanvas/drawing/Circle.kt
@@ -6,8 +6,8 @@ import android.view.MotionEvent
 import kotlin.math.abs
 
 class Circle(override val paint: Paint, override val invalidate: () -> Unit) : Drawing {
-    private var prevX: Float = 0f
-    private var prevY: Float = 0f
+    private var startX: Float = 0f
+    private var startY: Float = 0f
     private var cx: Float = 0f
     private var cy: Float = 0f
     private var radius: Float = 0f
@@ -19,17 +19,17 @@ class Circle(override val paint: Paint, override val invalidate: () -> Unit) : D
     override fun onTouchEvent(event: MotionEvent): Boolean {
         return when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                prevX = event.x
-                prevY = event.y
+                startX = event.x
+                startY = event.y
                 true
             }
 
             MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
-                val isToLeft = (event.x - prevX) < 0
-                val isToTop = (event.y - prevY) < 0
-                radius = abs(event.x - prevX) / 2
-                cx = if (isToLeft) prevX - radius else prevX + radius
-                cy = if (isToTop) prevY - radius else prevY + radius
+                val isToLeft = (event.x - startX) < 0
+                val isToTop = (event.y - startY) < 0
+                radius = abs(event.x - startX) / 2
+                cx = if (isToLeft) startX - radius else startX + radius
+                cy = if (isToTop) startY - radius else startY + radius
                 invalidate()
                 true
             }
