80f06e3c9291c3bc3ba1f23ac24d1165785575b1
vrexpert <vrexpert2302@gmail.com>
2023-10-10 16:07:53 +0900

    refactor: 직사각형 그리기 paintTool 정의

---
+++ app/src/main/java/woowacourse/paint/presentation/ui/canvas/shape/Rectangle.kt
@@ -0,0 +1,39 @@
+package woowacourse.paint.presentation.ui.canvas.shape
+
+import android.graphics.Paint
+import android.graphics.Path
+import woowacourse.paint.presentation.ui.canvas.Palette
+
+class Rectangle(palette: Palette) : PaintTool(palette, null) {
+
+    private var preX = INITIAL_X
+    private var preY = INITIAL_Y
+
+    init {
+        palette.paint.style = Paint.Style.FILL
+    }
+
+    override fun changePalette(palette: Palette): PaintTool = Rectangle(palette)
+
+    override fun nextPath(): PaintTool = Rectangle(this.palette)
+
+    override fun onMoveEvent(pointX: Float, pointY: Float) {
+        path.reset()
+        val left = minOf(preX, pointX)
+        val top = minOf(preY, pointY)
+        val bottom = maxOf(preY, pointY)
+        val right = maxOf(preX, pointX)
+        path.addRect(left, top, right, bottom, Path.Direction.CW)
+    }
+
+    override fun onDownEvent(pointX: Float, pointY: Float) {
+        preX = pointX
+        preY = pointY
+        path.moveTo(pointX, pointY)
+    }
+
+    companion object {
+        private const val INITIAL_X = 0f
+        private const val INITIAL_Y = 0f
+    }
+}
