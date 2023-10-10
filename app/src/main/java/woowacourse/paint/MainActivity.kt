f64641110959979953fb24ba5204d12766db31e1
Yerin <myheaven_b@naver.com>
2023-10-11 02:03:39 +0900

    fix: 불필요한 툴바 영역 제거

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -59,6 +59,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_activity, menu)
+
         return super.onCreateOptionsMenu(menu)
     }
 
