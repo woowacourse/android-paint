ce2c284830a32034e8b21f8746944185ecea5cb1
hyemdooly <hyemdooly@gmail.com>
2023-10-11 17:56:49 +0900

    feat: 태블릿 세로 모드 대응

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -94,20 +94,8 @@ class MainActivity : AppCompatActivity() {
     private fun setupColors() {
         binding.rvColors.adapter = adapter
         binding.rvColors.setHasFixedSize(true)
-        binding.rvColors.addItemDecoration(SpaceItemDecoration(getSpace()))
+        binding.rvColors.addItemDecoration(SpaceItemDecoration(25))
     }
-
-    private fun getSpace(): Int {
-        val colorWidth = resources.getDimensionPixelSize(R.dimen.color_item_size)
-        val display = this.applicationContext?.resources?.displayMetrics
-        val deviceWidth = display?.widthPixels
-
-        deviceWidth?.let {
-            return (deviceWidth - (colorWidth * 5)) / 4
-        }
-        return 10
-    }
-
     private fun setupWidthSlider() {
         binding.rsWidthChanger.valueFrom = MainViewModel.MIN_WIDTH
         binding.rsWidthChanger.valueTo = MainViewModel.MAX_WIDTH
