cd6ff78434fa1b4d13135b0d225d4de1a876e32a
hyemdooly <hyemdooly@gmail.com>
2023-10-11 13:21:04 +0900

    design: 도구에 따른 설정 화면 변경

--- app/src/main/java/woowacourse/paint/PaintChangingState.kt
+++ app/src/main/java/woowacourse/paint/PaintChangingState.kt
@@ -1,5 +1,5 @@
 package woowacourse.paint
 
 enum class PaintChangingState {
-    NOTHING, COLOR_CHANGING, WIDTH_CHANGING, TOOL_CHANGING
+    NOTHING, COLOR_CHANGING, WIDTH_CHANGING
 }
--- app/src/main/java/woowacourse/paint/canvas/Tool.kt
+++ app/src/main/java/woowacourse/paint/canvas/Tool.kt
@@ -6,20 +6,20 @@ import woowacourse.paint.canvas.drawing.Drawing
 import woowacourse.paint.canvas.drawing.Line
 import woowacourse.paint.canvas.drawing.Rectangle
 
-enum class Tool {
-    PEN {
+enum class Tool(val hasWidth: Boolean, val hasColor: Boolean) {
+    PEN(true, true) {
         override fun draw(paint: Paint, invalidate: () -> Unit): Drawing =
             Line.of(paint, invalidate)
     },
-    RECTANGLE {
+    RECTANGLE(false, true) {
         override fun draw(paint: Paint, invalidate: () -> Unit): Drawing =
             Rectangle.of(paint, invalidate)
     },
-    CIRCLE {
+    CIRCLE(false, true) {
         override fun draw(paint: Paint, invalidate: () -> Unit): Drawing =
             Circle.of(paint, invalidate)
     },
-    ERASER {
+    ERASER(true, false) {
         override fun draw(paint: Paint, invalidate: () -> Unit): Drawing =
             Line.of(paint, invalidate)
     }, ;
