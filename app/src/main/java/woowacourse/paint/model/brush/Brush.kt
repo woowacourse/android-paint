663001f54546d2ee14b635cd91b70f56f0cd9317
kwakjoohyeong <rhkrwngud445@gmail.com>
2023-10-11 16:16:57 +0900

    refactor: sealed interface로 변경

--- app/src/main/java/woowacourse/paint/model/brush/Brush.kt
+++ app/src/main/java/woowacourse/paint/model/brush/Brush.kt
@@ -3,18 +3,18 @@ package woowacourse.paint.model.brush
 import android.graphics.Paint
 import androidx.annotation.ColorInt
 
-sealed class Brush {
-    abstract val paintInstance: Paint
-    abstract fun updateStyle(paint: Paint)
+sealed interface Brush {
+    val paintInstance: Paint
+    fun updateStyle(paint: Paint)
 
-    abstract fun onActionDown(xCursor: Float, yCursor: Float)
+    fun onActionDown(xCursor: Float, yCursor: Float)
 
-    abstract fun onActionMove(xCursor: Float, yCursor: Float)
+    fun onActionMove(xCursor: Float, yCursor: Float)
 
-    abstract fun onActionUp(xCursor: Float, yCursor: Float)
+    fun onActionUp(xCursor: Float, yCursor: Float)
 
-    abstract fun updateColor(@ColorInt color: Int)
+    fun updateColor(@ColorInt color: Int)
 
-    abstract fun updateThickness(thickness: Float)
+    fun updateThickness(thickness: Float)
     fun copyPaint(): Paint = Paint().apply { set(paintInstance) }
 }
