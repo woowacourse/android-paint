43a0f2b08a36c4aa784c1fc21c432947b99a0c33
whk06061 <gju06061@naver.com>
2023-10-12 17:13:11 +0900

    feat: 원이 네 방향으로 그려지도록 구현

--- app/src/main/java/woowacourse/paint/drawing/DrawingCircle.kt
+++ app/src/main/java/woowacourse/paint/drawing/DrawingCircle.kt
@@ -4,6 +4,8 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import woowacourse.paint.ui.PathPoint
+import kotlin.math.max
+import kotlin.math.min
 
 class DrawingCircle : DrawingShape {
     private var startPoint: PathPoint = PathPoint(0f, 0f)
@@ -37,12 +39,11 @@ class DrawingCircle : DrawingShape {
     private fun processDirectionalLogic(
         logic: (startX: Float, startY: Float, radius: Float) -> Unit,
     ) {
-        if (startPoint.x < endPoint.x) {
-            val radius = endPoint.x - startPoint.x
-            logic(startPoint.x + radius, startPoint.y + radius, radius)
-        } else {
-            val radius = startPoint.x - endPoint.x
-            logic(startPoint.x - radius, startPoint.y - radius, radius)
-        }
+        val startX = (startPoint.x + endPoint.x) / 2
+        val startY = (startPoint.y + endPoint.y) / 2
+        val maxX = max(startPoint.x, endPoint.x)
+        val minX = min(startPoint.x, endPoint.x)
+        val radius = maxX - minX
+        logic(startX, startY, radius)
     }
 }
