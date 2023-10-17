02a00c50656e69adad6354f66692c2ed02ad615f
토마스 <123928686+ki960213@users.noreply.github.com>
2023-10-17 21:25:40 +0900

    [토마스] 뷰 챌린지 미션 3단계 제출합니다. (#67)
    * fix(Brush): 직사각형을 어떤 방향이든 그릴 수 있도록 수정
    
    * feat(AndroidManifest.xml): 세로 고정 모드 해제
    
    * feat: 다크 모드 대응
    
    * feat: 태블릿 세로 모드 대응
    
    * feat: 태블릿 가로 모드 대응
    
    * fix: 구성 변경 시 펜 굵기 유지하도록 수정
    
    * refactor(MainUiState): 매직 넘버 제거
    
    * feat(themes.xml): 다크 모드일 때 상태 바 색상을 검정색이 되도록 변경

--- app/src/main/java/woowacourse/paint/presentation/ui/uiState/MainUiState.kt
+++ app/src/main/java/woowacourse/paint/presentation/ui/uiState/MainUiState.kt
@@ -7,9 +7,17 @@ import woowacourse.paint.presentation.ui.model.SettingMode
 data class MainUiState(
     val settingMode: SettingMode,
     val color: Color,
+    val thickness: Float,
     val brush: Brush,
 ) {
     companion object {
-        val Initial: MainUiState = MainUiState(SettingMode.NONE, Color.RED, Brush.PEN)
+        private const val INIT_THICKNESS: Float = 0f
+
+        val Initial: MainUiState = MainUiState(
+            settingMode = SettingMode.NONE,
+            color = Color.RED,
+            thickness = INIT_THICKNESS,
+            brush = Brush.PEN,
+        )
     }
 }
