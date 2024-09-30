62a4379fd5f32a4ee6682bdc832dcac1a4c9cca4
김진우 <jinwoo3661@naver.com>
2024-09-30 21:13:29 +0900

    feat: line 모델 생성

---
+++ app/src/main/java/woowacourse/paint/brush/Line.kt
@@ -0,0 +1,49 @@
+package woowacourse.paint.brush
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+
+data class Line(
+    val path: Path = Path(),
+    val paint: Paint = Paint(),
+) {
+    private var linePath = path
+    private var linePaint = paint
+    private var endX = DEFAULT_POINT
+    private var endY = DEFAULT_POINT
+
+    init {
+        paint.style = Paint.Style.STROKE
+        paint.strokeJoin = Paint.Join.ROUND
+        paint.strokeCap = Paint.Cap.ROUND
+    }
+
+    fun changeColor(colorPalette: ColorPalette) {
+        paint.color = colorPalette.color
+    }
+
+    fun changeWidth(width: Float) {
+        paint.strokeWidth = width
+    }
+
+    fun move(x: Float, y: Float) {
+        endX = x
+        endY = y
+        path.moveTo(endX, endY)
+    }
+
+    fun quad(x: Float, y: Float) {
+        path.quadTo(endX, endY, (x + endX) / 2, (y + endY) / 2)
+        endX = x
+        endY = y
+    }
+
+    fun draw(canvas: Canvas) {
+        canvas.drawPath(linePath, linePaint)
+    }
+
+    companion object{
+        private const val DEFAULT_POINT = 0f
+    }
+}
