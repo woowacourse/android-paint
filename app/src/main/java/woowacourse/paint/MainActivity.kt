177e0ded5a41bf80c1cea8e6d2e0f55fdaff8225
Hogu59 <dbstjdguswkd@naver.com>
2024-09-30 19:10:51 +0900

    feat(Main): implement activity logic

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -2,13 +2,36 @@ package woowacourse.paint
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.content.ContextCompat
+import woowacourse.paint.databinding.ActivityMainBinding
+import woowacourse.paint.model.PaintingColor
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val paintingColorList = PaintingColor.values().toList()
+    private val paintingColorAdapter by lazy {
+        PaintingColorAdapter(paintingColorList) { color ->
+            binding.customView.changeColor(
+                ContextCompat.getColor(
+                    this,
+                    color.colorRes,
+                ),
+            )
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
+        binding.rvColors.adapter = paintingColorAdapter
+        binding.rangeSlider
+            .apply {
+                values = listOf(5f)
+                valueFrom = 1f
+                valueTo = 20f
+            }.addOnChangeListener { _, value, _ ->
+                binding.customView.changeStrokeWidth(value)
+            }
     }
 }
