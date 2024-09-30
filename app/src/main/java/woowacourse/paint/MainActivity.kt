4bfc1a55e92d1d759cba77e0e5dc802b2a714961
김진우 <jinwoo3661@naver.com>
2024-09-30 21:13:48 +0900

    feat: viewBinding

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -1,18 +1,38 @@
 package woowacourse.paint
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import woowacourse.paint.brush.Brush
+import woowacourse.paint.brush.ColorPalette
+import woowacourse.paint.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityViewBinding: ActivityMainBinding
+    private var brush: Brush = Brush()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityViewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityViewBinding.root)
+        initPalettes()
+        initListener()
+    }
 
-        val name = "레아"
-        val text = findViewById<TextView>(R.id.text)
-        text.text = "$name 안녕하세요!"
+    private fun initPalettes() {
+        activityViewBinding.customCanvas.changeColor(brush)
+        activityViewBinding.rvColor.adapter = ColorPaletteAdapter(
+            colorPalettes = ColorPalette.entries,
+        ) { color ->
+            brush = brush.changeColor(color)
+            activityViewBinding.customCanvas.changeColor(brush)
+        }
+    }
+
+    private fun initListener() {
+        activityViewBinding.rangeSlider.addOnChangeListener { _, width, _ ->
+            brush = brush.changeWidth(width)
+            activityViewBinding.customCanvas.changeLineWidth(brush)
+        }
     }
 }
---
+++ app/src/main/java/woowacourse/paint/canvas/CanvasView.kt
@@ -0,0 +1,73 @@
+package woowacourse.paint.canvas
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import woowacourse.paint.brush.Brush
+import woowacourse.paint.brush.ColorPalette
+import woowacourse.paint.brush.Line
+
+class CanvasView(
+    context: Context,
+    attributeSet: AttributeSet,
+) : View(context, attributeSet) {
+    private val lines: MutableList<Line> = mutableListOf()
+    private var drawingLine = Line()
+
+    init {
+        isFocusable = true
+        isFocusableInTouchMode = true
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        lines.forEach { it.draw(canvas) }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                startDrawing(event.x, event.y)
+                invalidate()
+                true
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                quadDrawing(event.x, event.y)
+                invalidate()
+                true
+            }
+
+            else -> super.onTouchEvent(event)
+        }
+    }
+
+    private fun startDrawing(x: Float, y: Float) {
+        drawingLine.move(x, y)
+        lines.add(drawingLine)
+    }
+
+    private fun quadDrawing(x: Float, y: Float) {
+        drawingLine.quad(x, y)
+    }
+
+    fun changeLineWidth(brush: Brush) {
+        drawingLine = Line(paint = createNewPaint(brush))
+    }
+
+    fun changeColor(brush: Brush) {
+        drawingLine = Line(paint = createNewPaint(brush))
+    }
+
+    private fun createNewPaint(brush: Brush): Paint{
+        return Paint().apply {
+            this.color = brush.colorPalette.color
+            this.strokeWidth = brush.width
+        }
+    }
+}
