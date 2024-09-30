2539933eeb58057afbbfe79522d72d456a7539f9
songpink <jejeje04@naver.com>
2024-09-30 19:33:42 +0900

    feat: 그림 그리기 기능 구현

---
+++ app/src/main/java/woowacourse/paint/CustomView.kt
@@ -0,0 +1,67 @@
+package woowacourse.paint
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class CustomView(
+    context: Context,
+    attrs: AttributeSet,
+) : View(context, attrs) {
+    private val path = Path()
+    private val paint = Paint()
+
+    init {
+        isFocusable = true
+        isFocusableInTouchMode = true
+        setupPaint()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawPath(path, paint)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val pointX = event.x
+        val pointY = event.y
+        when (event.action) {
+            MotionEvent.ACTION_DOWN ->
+                path.addOval(
+                    pointX - OVAL_SIZE,
+                    pointY - OVAL_SIZE,
+                    pointX + OVAL_SIZE,
+                    pointY + OVAL_SIZE,
+                    Path.Direction.CW,
+                )
+
+            MotionEvent.ACTION_MOVE ->
+                path.addOval(
+                    pointX - OVAL_SIZE,
+                    pointY - OVAL_SIZE,
+                    pointX + OVAL_SIZE,
+                    pointY + OVAL_SIZE,
+                    Path.Direction.CW,
+                )
+
+            else -> super.onTouchEvent(event)
+        }
+        invalidate()
+        return true
+    }
+
+    private fun setupPaint() {
+        paint.color = Color.RED
+    }
+
+    companion object {
+        private const val OVAL_SIZE = 10
+    }
+}
--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -1,16 +1,11 @@
 package woowacourse.paint
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val name = "레아"
-        val text = findViewById<TextView>(R.id.text)
-        text.text = "$name 안녕하세요!"
     }
 }
