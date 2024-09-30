dc3ad0457d923450fd20a2c6660ee019e93710c0
김진우 <jinwoo3661@naver.com>
2024-09-30 21:13:38 +0900

    feat: brush 생성

---
+++ app/src/main/java/woowacourse/paint/brush/Brush.kt
@@ -0,0 +1,19 @@
+package woowacourse.paint.brush
+
+data class Brush(
+    val colorPalette: ColorPalette = ColorPalette.RED,
+    val width: Float = DEFAULT_BRUSH_WIDTH,
+) {
+
+    fun changeColor(colorPalette: ColorPalette) : Brush{
+        return this.copy(colorPalette = colorPalette)
+    }
+
+    fun changeWidth(width: Float) : Brush{
+        return this.copy(width = width)
+    }
+
+    companion object{
+        private const val DEFAULT_BRUSH_WIDTH = 10f
+    }
+}
