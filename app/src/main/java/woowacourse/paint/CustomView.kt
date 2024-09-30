ac287692e24277db57d3b415d6770212115ddc8c
Hogu59 <dbstjdguswkd@naver.com>
2024-09-30 19:10:36 +0900

    feat(CustomView): implement CustomView

---
+++ app/src/main/java/woowacourse/paint/CustomView.kt
@@ -0,0 +1,73 @@
+package woowacourse.paint
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class CustomView(
+    context: Context,
+    attrs: AttributeSet,
+) : View(context, attrs) {
+    private val map: MutableMap<Path, Paint> = mutableMapOf()
+
+    private var path = Path()
+    private var paint = Paint()
+
+    init {
+        isFocusable = true
+        isFocusableInTouchMode = true
+        setupPaint()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        for ((path, paint) in map) {
+            canvas.drawPath(path, paint)
+        }
+        canvas.drawPath(path, paint)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val pointX = event.x
+        val pointY = event.y
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                path = Path()
+                path.moveTo(pointX, pointY)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                path.lineTo(pointX, pointY)
+            }
+
+            MotionEvent.ACTION_UP -> {
+                map[path] = Paint(paint)
+            }
+
+            else -> super.onTouchEvent(event)
+        }
+        invalidate()
+        return true
+    }
+
+    private fun setupPaint() {
+        paint.color = Color.RED
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 5f
+    }
+
+    fun changeColor(color: Int) {
+        paint.color = color
+    }
+
+    fun changeStrokeWidth(width: Float) {
+        paint.strokeWidth = width
+    }
+}
