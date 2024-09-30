d149cb3802975af1b0106bead91e0d17656a3506
dpcks0509 <dpcks0509@gmail.com>
2024-09-30 17:26:39 +0900

    feat: 그림판 UI 작성

---
+++ app/src/main/java/woowacourse/paint/PaintActivity.kt
@@ -0,0 +1,13 @@
+package woowacourse.paint
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class PaintActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paint)
+    }
+}
---
+++ app/src/main/java/woowacourse/paint/PaintBoard.kt
@@ -0,0 +1,8 @@
+package woowacourse.paint
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PaintBoard constructor(context: Context, attr: AttributeSet) : View(context, attr) {
+}
