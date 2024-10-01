74d589058f9434c360e515e1e482cf92c2732c44
songpink <jejeje04@naver.com>
2024-10-01 20:51:00 +0900

    feat: 색상, 굵기 변경 기능 구현

--- app/src/main/java/woowacourse/paint/CustomView.kt
+++ app/src/main/java/woowacourse/paint/CustomView.kt
@@ -14,8 +14,9 @@ class CustomView(
     context: Context,
     attrs: AttributeSet,
 ) : View(context, attrs) {
-    private val path = Path()
-    private val paint = Paint()
+    private val paths = mutableListOf<PathData>()
+    private var currentPath = Path()
+    val paint = Paint()
 
     init {
         isFocusable = true
@@ -25,43 +26,56 @@ class CustomView(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.drawPath(path, paint)
+        val currentColor = paint.color
+        val currentStrokeWidth = paint.strokeWidth
+        for (pathData in paths) {
+            paint.color = pathData.color
+            paint.strokeWidth = pathData.strokeWidth
+            canvas.drawPath(pathData.path, paint)
+        }
+        paint.color = currentColor
+        paint.strokeWidth = currentStrokeWidth
+        canvas.drawPath(currentPath, paint)
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         val pointX = event.x
         val pointY = event.y
-        when (event.action) {
-            MotionEvent.ACTION_DOWN ->
-                path.addOval(
-                    pointX - OVAL_SIZE,
-                    pointY - OVAL_SIZE,
-                    pointX + OVAL_SIZE,
-                    pointY + OVAL_SIZE,
-                    Path.Direction.CW,
-                )
 
-            MotionEvent.ACTION_MOVE ->
-                path.addOval(
-                    pointX - OVAL_SIZE,
-                    pointY - OVAL_SIZE,
-                    pointX + OVAL_SIZE,
-                    pointY + OVAL_SIZE,
-                    Path.Direction.CW,
-                )
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                currentPath = Path()
+                currentPath.moveTo(pointX, pointY)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                currentPath.lineTo(pointX, pointY)
+            }
+
+            MotionEvent.ACTION_UP -> {
+                finishDrawing()
+            }
 
             else -> super.onTouchEvent(event)
         }
+
         invalidate()
         return true
     }
 
-    private fun setupPaint() {
-        paint.color = Color.RED
+    fun startDrawing() {
+        currentPath = Path()
     }
 
-    companion object {
-        private const val OVAL_SIZE = 10
+    private fun finishDrawing() {
+        paths.add(PathData(currentPath, paint.color, paint.strokeWidth))
+    }
+
+    private fun setupPaint() {
+        paint.color = Color.RED
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = 10f
+        paint.isAntiAlias = true
     }
 }
--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -1,11 +1,69 @@
 package woowacourse.paint
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.slider.RangeSlider
+import woowacourse.paint.databinding.ActivityMainBinding
+import woowacourse.paint.util.MyColor
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        initBinding()
+        setPaintColor()
+        setPaintWidth()
+    }
+
+    private fun setPaintColor() {
+        binding.btnColorRed.setOnClickListener {
+            binding.customView.startDrawing()
+            binding.customView.paint.color = MyColor.RED
+        }
+
+        binding.btnColorOrange.setOnClickListener {
+            binding.customView.startDrawing()
+            binding.customView.paint.color = MyColor.ORANGE
+            Log.d("alsong", "${binding.customView.paint.color}")
+        }
+
+        binding.btnColorYellow.setOnClickListener {
+            binding.customView.startDrawing()
+            binding.customView.paint.color = MyColor.YELLOW
+        }
+
+        binding.btnColorGreen.setOnClickListener {
+            binding.customView.startDrawing()
+            binding.customView.paint.color = MyColor.GREEN
+        }
+
+        binding.btnColorBlue.setOnClickListener {
+            binding.customView.startDrawing()
+            binding.customView.paint.color = MyColor.BLUE
+        }
+    }
+
+    private fun setPaintWidth() {
+        binding.rangeSlider.valueFrom = 0.0f
+        binding.rangeSlider.valueTo = 100.0f
+
+        binding.rangeSlider.addOnChangeListener(
+            RangeSlider.OnChangeListener { _, value, _ ->
+                binding.customView.paint.strokeWidth = value
+            },
+        )
+    }
+
+    private fun initBinding() {
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
