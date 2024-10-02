19a3cb706330773c934e64e02995a2267c45a9bb
hxeyexn <kck85075@kumoh.ac.kr>
2024-10-03 00:52:45 +0900

    refactor: Line 객체에게 색상이나 굵기를 변경하는 역할 부여

--- app/src/main/java/woowacourse/paint/drawingboard/DrawingBoard.kt
+++ app/src/main/java/woowacourse/paint/drawingboard/DrawingBoard.kt
@@ -3,21 +3,16 @@ package woowacourse.paint.drawingboard
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Paint.Cap
 import android.graphics.Path
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
-import androidx.core.content.ContextCompat
-import woowacourse.paint.R
 
 class DrawingBoard(context: Context, attrs: AttributeSet?) : View(context, attrs) {
-    private var selectedColor: Int = ContextCompat.getColor(context, R.color.red)
-    private var selectedStrokeWidth: Float = 1f
-
-    private var line = Line(Path(), setupPaint())
-    private val lines: MutableList<Line> = mutableListOf(line)
+    private var currentLine = Line(Path(), Paint().apply { color = DEFAULT_LINE_COLOR })
+    private val lines: MutableList<Line> = mutableListOf(currentLine)
 
     init {
         isFocusable = true
@@ -37,8 +32,11 @@ class DrawingBoard(context: Context, attrs: AttributeSet?) : View(context, attrs
         val pointY = event.y
 
         when (event.action) {
-            MotionEvent.ACTION_DOWN -> line.path.moveTo(pointX, pointY)
-            MotionEvent.ACTION_MOVE -> line.path.lineTo(pointX, pointY)
+            MotionEvent.ACTION_DOWN -> {
+                addNewLine()
+                currentLine.moveTo(pointX, pointY)
+            }
+            MotionEvent.ACTION_MOVE -> currentLine.lineTo(pointX, pointY)
             else -> return super.onTouchEvent(event)
         }
         invalidate()
@@ -46,26 +44,20 @@ class DrawingBoard(context: Context, attrs: AttributeSet?) : View(context, attrs
     }
 
     fun setupStrokeWidth(strokeWidth: Float) {
-        selectedStrokeWidth = strokeWidth
-        addNewLine()
+        val newStrokeWidthLine = currentLine.updateStrokeWidth(strokeWidth)
+        currentLine = newStrokeWidthLine
     }
 
     fun setupColor(color: Int) {
-        selectedColor = color
-        addNewLine()
+        val newColorLine = currentLine.updateColor(color)
+        currentLine = newColorLine
     }
 
     private fun addNewLine() {
-        line = Line(Path(), setupPaint())
-        lines.add(line)
+        lines.add(currentLine)
     }
 
-    private fun setupPaint() =
-        Paint().apply {
-            color = selectedColor
-            strokeCap = Cap.ROUND
-            style = Paint.Style.STROKE
-            strokeWidth = selectedStrokeWidth
-            isAntiAlias = true
-        }
+    companion object {
+        const val DEFAULT_LINE_COLOR = Color.RED
+    }
 }
--- app/src/main/java/woowacourse/paint/drawingboard/Line.kt
+++ app/src/main/java/woowacourse/paint/drawingboard/Line.kt
@@ -3,4 +3,41 @@ package woowacourse.paint.drawingboard
 import android.graphics.Paint
 import android.graphics.Path
 
-data class Line(val path: Path, val paint: Paint)
+class Line(val path: Path, val paint: Paint) {
+    init {
+        setupPaint()
+    }
+
+    fun moveTo(
+        pointX: Float,
+        pointY: Float,
+    ) {
+        path.moveTo(pointX, pointY)
+    }
+
+    fun lineTo(
+        pointX: Float,
+        pointY: Float,
+    ) {
+        path.lineTo(pointX, pointY)
+    }
+
+    fun updateStrokeWidth(strokeWidth: Float): Line {
+        val paint = Paint(paint)
+        paint.strokeWidth = strokeWidth
+        return Line(Path(), paint)
+    }
+
+    fun updateColor(color: Int): Line {
+        val paint = Paint(paint)
+        paint.color = color
+        return Line(Path(), paint)
+    }
+
+    private fun setupPaint() =
+        paint.apply {
+            strokeCap = Paint.Cap.ROUND
+            style = Paint.Style.STROKE
+            isAntiAlias = true
+        }
+}
