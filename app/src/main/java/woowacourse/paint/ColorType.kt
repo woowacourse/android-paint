5ee6d88b0e0723b56a6dcad89942f6c3c4f9422d
gaeun5744 <geun5744@gmail.com>
2024-10-02 14:55:47 +0900

    feat: ColorType 구현

---
+++ app/src/main/java/woowacourse/paint/ColorType.kt
@@ -0,0 +1,12 @@
+package woowacourse.paint
+
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+
+enum class ColorType(@ColorRes val colorId: Int) {
+    RED(R.color.red),
+    ORANGE(R.color.orange),
+    YELLOW(R.color.yellow),
+    GREEN(R.color.green),
+    BLUE(R.color.blue)
+}
