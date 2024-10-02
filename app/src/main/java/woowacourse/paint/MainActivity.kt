34b115f34e6c4d99cf17b91c9f9609fe1a7233c0
hxeyexn <kck85075@kumoh.ac.kr>
2024-10-02 16:55:02 +0900

    refactor: drawingBoard를 lazy를 이용해 지연 초기화하도록 변경
    - 이전: lateinit 사용
    - 이후: lazy 사용

--- app/src/main/java/woowacourse/paint/MainActivity.kt
+++ app/src/main/java/woowacourse/paint/MainActivity.kt
@@ -10,22 +10,17 @@ import woowacourse.paint.drawingboard.DrawingBoard
 class MainActivity : AppCompatActivity(), ColorPaletteHandler {
     private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
     private val adapter: ColorPaletteAdapter by lazy { ColorPaletteAdapter(colorPaletteHandler = this) }
-    private lateinit var drawingBoard: DrawingBoard
+    private val drawingBoard: DrawingBoard by lazy { binding.customViewMainDrawingBoard }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupDrawingBoard()
         setupColorPalette()
         setupStrokeWidthRangeSlider()
     }
 
     override fun onColorChangeClicked(color: Int) {
         drawingBoard.setupColor(color)
-    }
-
-    private fun setupDrawingBoard() {
-        drawingBoard = binding.customViewMainDrawingBoard
     }
 
     private fun setupColorPalette() {
