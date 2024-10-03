c60dc00715eeb4128b060f170ec7053cecf43576
chaehyun <80222352+chaehyuns@users.noreply.github.com>
2024-10-03 13:43:29 +0900

    style: ktlint 적용

--- app/src/main/java/woowacourse/paint/BindinAdapters.kt
+++ app/src/main/java/woowacourse/paint/BindinAdapters.kt
@@ -6,7 +6,10 @@ import androidx.databinding.BindingAdapter
 import woowacourse.paint.model.ColorPalette
 
 @BindingAdapter("backgroundTint")
-fun setBackgroundTint(view: View, colorPalette: ColorPalette) {
+fun setBackgroundTint(
+    view: View,
+    colorPalette: ColorPalette,
+) {
     val color = ContextCompat.getColor(view.context, colorPalette.colorResId)
     view.backgroundTintList = android.content.res.ColorStateList.valueOf(color)
 }
--- app/src/main/java/woowacourse/paint/model/ColorPalette.kt
+++ app/src/main/java/woowacourse/paint/model/ColorPalette.kt
@@ -5,12 +5,15 @@ import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import woowacourse.paint.R
 
-enum class ColorPalette(@ColorRes val colorResId: Int){
+enum class ColorPalette(
+    @ColorRes val colorResId: Int,
+) {
     RED(colorResId = R.color.red),
     ORANGE(colorResId = R.color.orange),
     YELLOW(colorResId = R.color.yellow),
     GREEN(colorResId = R.color.green),
-    BLUE(colorResId = R.color.blue);
+    BLUE(colorResId = R.color.blue),
+    ;
 
     @ColorInt
     fun getColor(context: Context): Int {
