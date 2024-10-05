1dd6d19c6536d516c1a24400639d0aa045ebd308
somin <salth0601@gmail.com>
2024-10-05 13:00:50 +0900

    feat: 사각형 그리는 기능 구현

--- app/src/main/java/woowacourse/paint/PaintCanvasView.kt
+++ app/src/main/java/woowacourse/paint/PaintCanvasView.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -14,14 +15,16 @@ import androidx.core.content.ContextCompat
 class PaintCanvasView(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private var selectedColorInt = ContextCompat.getColor(context, default_color)
     private var selectedStrokeWidth = DEFAULT_STROKE_WIDTH
-    private var selectedDiagram = Diagram.LINE
-
-    private var startX = 0F
-    private var startY = 0F
-    private var diagramPath = Path()
     private var path = Path()
     private var paint = createPaintWith(selectedColorInt, selectedStrokeWidth)
     private val canvasData = mutableListOf(path to paint)
+
+    private var selectedDiagram = Diagram.LINE
+    private var startX = 0F
+    private var startY = 0F
+    private var rect = RectF()
+    private var diagramPaint = createDiagramPaintWith(selectedColorInt)
+    private val canvasDiagramData = mutableListOf(rect to diagramPaint)
 
     init {
         isFocusable = true
@@ -47,7 +50,10 @@ class PaintCanvasView(context: Context, attrs: AttributeSet) : View(context, att
         canvasData.forEach {
             canvas.drawPath(it.first, it.second)
         }
-        canvas.drawPath(diagramPath, createPaintWith(selectedColorInt, selectedStrokeWidth))
+        canvasDiagramData.forEach {
+            canvas.drawRect(it.first, it.second)
+        }
+        canvas.drawRect(rect, diagramPaint)
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -55,44 +61,67 @@ class PaintCanvasView(context: Context, attrs: AttributeSet) : View(context, att
         val pointX = event.x
         val pointY = event.y
         when (event.action) {
-            MotionEvent.ACTION_DOWN -> startNewDiagram(pointX, pointY)
-            MotionEvent.ACTION_MOVE -> drawLine(pointX, pointY)
+            MotionEvent.ACTION_DOWN -> startDrawing(pointX, pointY)
+            MotionEvent.ACTION_MOVE -> progressDrawing(pointX, pointY)
+            MotionEvent.ACTION_UP -> finishDrawing(pointX, pointY)
             else -> super.onTouchEvent(event)
         }
         invalidate()
         return true
     }
 
-    private fun startNewDiagram(
+    private fun startDrawing(
         pointX: Float,
         pointY: Float,
     ) {
         when (selectedDiagram) {
             Diagram.LINE -> {
-                path = Path().apply { moveTo(pointX, pointY) }
-                canvasData.add(path to createPaintWith(selectedColorInt, selectedStrokeWidth))
+                startLine(pointX, pointY)
             }
 
             Diagram.RECT -> {
-                startX = pointX
-                startY = pointY
+                startRect(pointX, pointY)
             }
         }
     }
 
-    private fun drawLine(
+    private fun startLine(pointX: Float, pointY: Float) {
+        path = Path().apply { moveTo(pointX, pointY) }
+        canvasData.add(path to createPaintWith(selectedColorInt, selectedStrokeWidth))
+    }
+
+    private fun startRect(pointX: Float, pointY: Float) {
+        startX = pointX
+        startY = pointY
+        diagramPaint = createDiagramPaintWith(selectedColorInt)
+    }
+
+    private fun progressDrawing(
         pointX: Float,
         pointY: Float,
     ) {
         when (selectedDiagram) {
             Diagram.LINE -> {
-                path.lineTo(pointX, pointY)
+                progressLine(pointX, pointY)
             }
 
             Diagram.RECT -> {
-                diagramPath =
-                    Path().apply { addRect(startX, startY, pointX, pointY, Path.Direction.CW) }
+                rect = RectF(startX, startY, pointX, pointY)
             }
+        }
+    }
+
+    private fun progressLine(pointX: Float, pointY: Float) {
+        path.lineTo(pointX, pointY)
+    }
+
+    private fun finishDrawing(
+        pointX: Float,
+        pointY: Float,
+    ) {
+        if (selectedDiagram == Diagram.RECT) {
+            rect = RectF(startX, startY, pointX, pointY)
+            canvasDiagramData.add(rect to diagramPaint)
         }
     }
 
@@ -106,6 +135,14 @@ class PaintCanvasView(context: Context, attrs: AttributeSet) : View(context, att
         color = colorValue
         strokeWidth = width
     }
+
+    private fun createDiagramPaintWith(colorValue: Int) =
+        Paint().apply {
+            strokeCap = Paint.Cap.ROUND
+            isAntiAlias = true
+            style = Paint.Style.FILL
+            color = colorValue
+        }
 
     companion object {
         private val default_color = R.color.red
