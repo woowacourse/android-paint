3fdb1a1fe4c421001e5d4b1cb67858fd67a5a3e2
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-06 22:14:10 +0900

    refactor: Stroke 경로 리스트 방어적 복사

--- app/src/main/java/woowacourse/paint/model/Strokes.kt
+++ app/src/main/java/woowacourse/paint/model/Strokes.kt
@@ -3,7 +3,7 @@ package woowacourse.paint.model
 class Strokes(value: MutableList<Stroke> = mutableListOf()) {
     private val _value: MutableList<Stroke> = value
     val value: List<Stroke>
-        get() = _value
+        get() = _value.toList()
 
     fun add(stroke: Stroke) {
         _value.add(stroke)
