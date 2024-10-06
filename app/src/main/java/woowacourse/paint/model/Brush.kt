602308da4e78f39b18a6a8bae4babcfa59ca4ac5
kkosang <eiwoqp77@gmail.com>
2024-10-07 02:51:00 +0900

    feat: Brush 타입 선택

--- app/src/main/java/woowacourse/paint/model/Brush.kt
+++ app/src/main/java/woowacourse/paint/model/Brush.kt
@@ -3,7 +3,13 @@ package woowacourse.paint.model
 data class Brush(
     val strokeWidth: Float = DEFAULT_STROKE_WIDTH,
     val color: Int = DEFAULT_COLOR,
+    val brushType: BrushType = DEFAULT_BRUSH_TYPE,
 ) {
+
+    fun update(updateBrush: Brush.() -> Brush): Brush {
+        return this.updateBrush()
+    }
+
     fun changeWidth(width: Float): Brush {
         return copy(strokeWidth = width)
     }
@@ -12,8 +18,20 @@ data class Brush(
         return copy(color = color)
     }
 
+    fun changeBrushType(brushType: BrushType): Brush {
+        return copy(brushType = brushType)
+    }
+
     companion object {
         const val DEFAULT_STROKE_WIDTH = 10f
         const val DEFAULT_COLOR = 0xFF000000.toInt()
+        val DEFAULT_BRUSH_TYPE = BrushType.PENCIL
     }
+}
+
+enum class BrushType {
+    PENCIL,
+    ERASER,
+    CIRCLE,
+    SQUARE
 }
