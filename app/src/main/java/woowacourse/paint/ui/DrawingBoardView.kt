7b233c12ea330463459420b9ba3f4fa620480ffc
Junyoung-WON <wjyo60@gmail.com>
2024-10-06 13:39:16 +0900

    feat: (2단계) 그리기 모드에 따라 그림을 그리는 방법 변경하는 기능 구현
    - drawingMode 에 따라 그리기 방식 분기 처리
    - setLayerType : 하드웨어 가속 설정으로 지우개 구현 및 draw 연산 속도 개선 시도
    - ACTION_DOWN 시 터치한 위치를 저장하여, 해당 위치를 기준으로 도형 그리기
    - 원, 사각형을 그리는 로직 구현
    - ACTION_DOWN 시에 그림을 추가 및 onDraw 의 canvas draw 로직 변경
    - 그리기 모드를 변경하는 메서드 작성
    - 메서드 분리 리팩터링

--- app/src/main/java/woowacourse/paint/ui/DrawingBoardView.kt
+++ app/src/main/java/woowacourse/paint/ui/DrawingBoardView.kt
@@ -9,34 +9,49 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import woowacourse.paint.model.Drawing
+import woowacourse.paint.model.DrawingMode
+import woowacourse.paint.model.DrawingMode.Companion.setPaintStyle
+import kotlin.math.max
+import kotlin.math.min
 
 class DrawingBoardView constructor(context: Context, attrs: AttributeSet) : View(context, attrs) {
     private val paint = Paint()
     private var path = Path()
     private val drawings = mutableListOf<Drawing>()
+    private var drawingMode: DrawingMode = DrawingMode.PEN
+    private var startX: Float = 0F
+    private var startY: Float = 0F
 
     init {
         isFocusable = true
         isFocusableInTouchMode = true
+        setLayerType(LAYER_TYPE_HARDWARE, null)
         initPaint()
+    }
+
+    private fun initPaint() {
+        paint.isAntiAlias = true
+        paint.style = Paint.Style.STROKE
+        paint.strokeJoin = Paint.Join.ROUND
+        paint.strokeCap = Paint.Cap.ROUND
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        drawings.forEach { canvas.drawPath(it.path, it.paint) }
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-                path =
-                    Path().apply {
-                        moveTo(event.x, event.y)
-                        lineTo(event.x, event.y)
-                    }
-                drawings.add(Drawing(path, Paint(paint)))
-                invalidate()
+                initStartPosition(event)
+                addNewDrawing(event)
+                drawByMode(event.x, event.y)
             }
 
             MotionEvent.ACTION_MOVE -> {
-                path.lineTo(event.x, event.y)
-                invalidate()
+                drawByMode(event.x, event.y)
             }
 
             MotionEvent.ACTION_UP -> {
@@ -48,17 +63,67 @@ class DrawingBoardView constructor(context: Context, attrs: AttributeSet) : View
         return true
     }
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        drawings.forEach { canvas.drawPath(it.path, it.paint) }
-        canvas.drawPath(path, paint)
+    private fun initStartPosition(event: MotionEvent) {
+        startX = event.x
+        startY = event.y
     }
 
-    private fun initPaint() {
-        paint.isAntiAlias = true
-        paint.style = Paint.Style.STROKE
-        paint.strokeJoin = Paint.Join.ROUND
-        paint.strokeCap = Paint.Cap.ROUND
+    private fun addNewDrawing(event: MotionEvent) {
+        path =
+            Path().apply {
+                moveTo(event.x, event.y)
+            }
+        drawings.add(Drawing(path, Paint(paint)))
+    }
+
+    private fun drawByMode(
+        currentX: Float,
+        currentY: Float,
+    ) {
+        when (drawingMode) {
+            DrawingMode.PEN -> {
+                drawLine(currentX, currentY)
+            }
+
+            DrawingMode.CIRCLE -> {
+                drawCircle(currentX, currentY)
+            }
+
+            DrawingMode.RECTANGLE -> {
+                drawRectangle(currentX, currentY)
+            }
+
+            DrawingMode.ERASE -> {
+                drawLine(currentX, currentY)
+            }
+        }
+        invalidate()
+    }
+
+    private fun drawLine(currentX: Float, currentY: Float) {
+        path.lineTo(currentX, currentY)
+    }
+
+    private fun drawCircle(currentX: Float, currentY: Float) {
+        path.reset()
+        path.addOval(
+            startX,
+            startY,
+            currentX,
+            currentY,
+            Path.Direction.CW,
+        )
+    }
+
+    private fun drawRectangle(currentX: Float, currentY: Float) {
+        path.reset()
+        path.addRect(
+            min(startX, currentX),
+            min(startY, currentY),
+            max(currentX, startX),
+            max(currentY, startY),
+            Path.Direction.CW,
+        )
     }
 
     fun setPaintColor(color: Int) {
@@ -67,5 +132,10 @@ class DrawingBoardView constructor(context: Context, attrs: AttributeSet) : View
 
     fun setPaintStrokeSize(size: Float) {
         paint.strokeWidth = size
+    }
+
+    fun setDrawingMode(mode: DrawingMode) {
+        drawingMode = mode
+        drawingMode.setPaintStyle(paint)
     }
 }
