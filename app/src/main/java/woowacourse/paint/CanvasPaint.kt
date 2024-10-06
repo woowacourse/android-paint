51cfadd833be5b9b73bb604acde52687742171c4
gaeun5744 <geun5744@gmail.com>
2024-10-06 22:13:21 +0900

    feat: 지우개 기능 추가

--- app/src/main/java/woowacourse/paint/CanvasPaint.kt
+++ app/src/main/java/woowacourse/paint/CanvasPaint.kt
@@ -1,12 +1,18 @@
 package woowacourse.paint
 
 import android.graphics.Paint
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffXfermode
+import android.graphics.Xfermode
 import androidx.annotation.ColorInt
 
 data class CanvasPaint(
     @ColorInt private val colorInt: Int,
     private val brushWidth: Float,
+    private val paintMode: PorterDuff.Mode = PorterDuff.Mode.SRC_OVER
 ) : Paint() {
+
+
     init {
         style = Style.STROKE
         isAntiAlias = true
@@ -14,6 +20,7 @@ data class CanvasPaint(
         strokeCap = Cap.ROUND
         strokeWidth = brushWidth
         color = colorInt
+        xfermode = PorterDuffXfermode(paintMode)
     }
 
     fun changeColor(
@@ -25,4 +32,17 @@ data class CanvasPaint(
     fun changeBrushWidth(brushWidth: Float): CanvasPaint {
         return this.copy(brushWidth = brushWidth)
     }
+
+    fun changeToEraseMode(): CanvasPaint {
+        return this.copy(paintMode = PorterDuff.Mode.CLEAR)
+    }
+
+    fun changeColorMode(): CanvasPaint {
+        return this.copy(paintMode = PorterDuff.Mode.SRC_OVER)
+    }
+
+    fun isEraseMode(): Boolean {
+        return paintMode == PorterDuff.Mode.CLEAR
+    }
+
 }
