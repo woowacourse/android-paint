9ef4c50f25c7d2f7f43242824b3e75ba27a64f1c
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2024-10-06 20:56:47 +0900

    feat: 사용자의 그림 기록을 관리하는 Strokes 일급 컬렉션 추가

---
+++ app/src/main/java/woowacourse/paint/model/Strokes.kt
@@ -0,0 +1,49 @@
+package woowacourse.paint.model
+
+import android.graphics.Path
+import android.graphics.RectF
+
+class Strokes(value: MutableList<Stroke> = mutableListOf()) {
+    private val _value: MutableList<Stroke> = value
+    val value: List<Stroke>
+        get() = _value
+
+    fun add(stroke: Stroke) {
+        _value.add(stroke)
+    }
+
+    fun removeAt(index: Int) {
+        _value.removeAt(index)
+    }
+
+    fun clear() {
+        _value.clear()
+    }
+
+    fun removeIntersectingStrokes(eraserPath: Path) {
+        val eraserBounds = RectF()
+        eraserPath.computeBounds(eraserBounds, true)
+        val strokesToRemove = mutableListOf<Int>()
+        value.forEachIndexed { index, stroke ->
+            val strokeBounds = RectF()
+            stroke.path.computeBounds(strokeBounds, true)
+            if (RectF.intersects(eraserBounds, strokeBounds) && isPathIntersecting(
+                    stroke.path,
+                    eraserPath
+                )
+            ) {
+                strokesToRemove.add(index)
+            }
+        }
+
+        strokesToRemove.asReversed().forEach { index ->
+            _value.removeAt(index)
+        }
+    }
+
+    private fun isPathIntersecting(path1: Path, path2: Path): Boolean {
+        val intersectionPath = Path()
+        intersectionPath.op(path1, path2, Path.Op.INTERSECT)
+        return !intersectionPath.isEmpty
+    }
+}
